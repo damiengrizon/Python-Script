4fdf17cced13b056e03ad207fc7218cb2f35abb7
damiengrizon <60382390+damiengrizon@users.noreply.github.com>
2021-11-29 21:51:44 +0100

    Add files via upload
    Structure d'une fonction en python

---
+++ recuperer_et_afficher_personne.py
@@ -0,0 +1,16 @@
+
+
+
+
+def recuperer_et_afficher_personne(nom="", prenom="", age=0):
+    print("La personne est" + nom + " son prénom est" + prenom + "et son age est " + str(age))
+
+
+def est_mineur():
+    if age < 18:
+        True 
+    False   
+
+recuperer_et_afficher_personne("jean", "jean", 18)
+recuperer_et_afficher_personne("jordan", "marie", 22)
+recuperer_et_afficher_personne("Artuhuer", "pierre", 12)
