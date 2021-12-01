b020504e91da76b7efb6cb5ea0947a2eba211375
damiengrizon <60382390+damiengrizon@users.noreply.github.com>
2021-12-01 21:56:42 +0100

    Add files via upload
    Enrichissement de la fonction avec d'autres fonctions imbriquées

--- recuperer_et_afficher_personne.py
+++ recuperer_et_afficher_personne.py
@@ -1,11 +1,23 @@
+def recuperer_infos_personne(numero_personne):
+    nom = input("nom de la personne : " + str(numero_personne) + " :")
+    age = input("age de la personne : " + str(numero_personne) + " :")
+    return nom, age
+
+def afficher_infos_personne(numero_personne, nom, age):
+    print("La personne "+ str(numero_personne) +" est " + nom + " et son age est de " + str(age)+ " an(s)")
+    print("le nom possède " +  str(len(nom)) + " caractères.")
 
 
+def recuperer_et_afficher_personne(numero_persone):
+    nom_personne, age_personne = recuperer_infos_personne(numero_persone)
+    afficher_infos_personne(numero_persone, nom_personne, age_personne)
 
+nombre_de_personne = int(input(" nombre de personne : "))
 
-def recuperer_et_afficher_personne(nom="", prenom="", age=0):
-    print("La personne est" + nom + " son prénom est" + prenom + "et son age est " + str(age))
+for i in range(nombre_de_personne):
+        recuperer_et_afficher_personne(i+1)
 
-
+'''
 def est_mineur():
     if age < 18:
         True 
@@ -14,3 +26,4 @@ def est_mineur():
 recuperer_et_afficher_personne("jean", "jean", 18)
 recuperer_et_afficher_personne("jordan", "marie", 22)
 recuperer_et_afficher_personne("Artuhuer", "pierre", 12)
+'''
