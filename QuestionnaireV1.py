e6d94acfa5d286ff86a65e26e99c33d835305632
damiengrizon <60382390+damiengrizon@users.noreply.github.com>
2022-02-03 22:01:19 +0100

    Add files via upload
    Re-factorisation du code afin d'implémenter des fonctions

--- QuestionnaireV1.py
+++ QuestionnaireV1.py
@@ -1,13 +1,18 @@
+def poserQuestion(Q,a,b,c,d,LettreBonneReponse):
+    print(Q)
+    print("(a)" + a )
+    print("(b)" + b )
+    print("(c)" + c )
+    print("(d)" + d )
+        
+    reponseQuestion = input ( "quelle est votre réponse?")
 
-question1 = "Qui est le plus rapide?"
-reponseA = "(a) le lapin"
-reponseB = "(b) le guépard"
-reponseC = "(c) la tortue"
-reponseD = "(d) le chien"
+    if reponseQuestion == LettreBonneReponse:
+        print("ouah t trop fort")
+    else:
+        print("Dommage, ce n'est pas la bonne réponse")
 
-print(question1)
-reponseQuestion = input ( "quelle est votre réponse?")
-if reponseQuestion == reponseB:
-    print("ouah t trop fort")
-else:
-    print("c'est la bonne réponse")
+poserQuestion("Qui est le plus rapide ? ","lapin", "renard", "pieuvre", "guépard", "d")
+poserQuestion("Qui est le plus lent ? ","lapin","tortue", "pieuvre", "guépard", "b")
+poserQuestion("Qui est le pénible ? ","lapin", "renard", "pieuvre", "chat", "d")
+poserQuestion("Qui est le plus grand ? ","lapin", "renard", "éléphant", "guépard", "c")
