f8116af29dc63afa2e4369465e1baf5c7471d45e
damiengrizon <60382390+damiengrizon@users.noreply.github.com>
2022-11-30 07:42:33 +0100

    Add files via upload
    essai pour les anneaux olympique en commande individuelle

---
+++ test_tkinter-2.py
@@ -0,0 +1,66 @@
+from tkinter import *
+
+def drawcircle():
+    # global x1, y1, x2, y2  # coordonnées de la ligne
+    x1, y1, x2, y2 = 10, 200, 210, 400  
+    "tracé d'un cercle dans le canevas can1"
+    can1.create_oval([x1, y1, x2, y2], width = 1, outline='green')
+
+def olympiqueRing():
+    x1, y1, x2, y2 = 10, 200, 210, 400  
+    x3, y3, x4, y4 = 110, 100, 310, 300
+    couleur = ['red', 'blue', 'black', 'green','pink', 'yellow', 'purple']
+
+    nbre_cercle = 7
+    while 7 >= nbre_cercle >= 4:
+        coul = couleur[nbre_cercle-1]
+        can1.create_oval([x1, y1, x2, y2], width = 2, outline = coul)
+        
+        x1, x2 = x1 +200, x2 +200
+        print(coul)
+        nbre_cercle -= 1
+
+    while 3 >= nbre_cercle > 0:
+        can1.create_oval([x3, y3, x4, y4 ], width = 2, outline = coul)  
+        x3, x4 = x3 +200, x4 +200
+        coul = couleur[nbre_cercle-2]
+        print(coul)
+        nbre_cercle -= 1
+
+def circleOne(cercle):
+    x1, y1, x2, y2 = 10, 200, 210, 400  
+    x3, y3, x4, y4 = 110, 100, 310, 300
+    couleur = ['red', 'blue', 'black', 'green','pink', 'yellow', 'purple']
+
+    if 7 >= cercle >= 4:
+        coul = couleur[cercle-1]
+        x1, x2 = x1 +200*cercle, x2 +200*cercle
+        can1.create_oval([x1, y1, x2, y2], width = 2, outline = coul)
+
+    if 3 >= cercle > 0:
+        x3, x4 = x3 +200*cercle, x4 +200*cercle
+        coul = couleur[cercle-2]
+        can1.create_oval([x3, y3, x4, y4 ], width = 2, outline = coul)  
+
+fen1 = Tk()
+
+
+# création des widgets "esclaves" :
+can1 = Canvas(fen1, bg = 'white', height=500, width=820)
+can1.pack(side=LEFT)
+bou1 = Button(fen1, text="quitter", command = fen1.quit)
+bou1.pack(side = BOTTOM)
+bou2 = Button(fen1, text="Tracer un cercle", command = drawcircle)
+bou2.pack()
+bou3 = Button(fen1, text="Tracer les anneaux", command = olympiqueRing)
+bou3.pack()
+bou4 = Button(fen1, text="Tracer 1", command = circleOne(1))
+bou4.pack()
+bou5 = Button(fen1, text="Tracer 2", command = circleOne(2))
+bou5.pack()
+bou6 = Button(fen1, text="Tracer 3", command = circleOne(3))
+bou6.pack()
+bou7 = Button(fen1, text="Tracer 4", command = circleOne(4))
+bou7.pack()
+fen1.mainloop() # démarrage du réceptionnaire d'évènement
+fen1.destroy() # destruction fermeture de la fenetre
